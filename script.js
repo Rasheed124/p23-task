bde7b526e5354da1cb14adfaa3cfd9991e650f01
Rasheed124 <rashdevatrealglobe@gmail.com>
2025-07-16 20:36:30 +0100

    task completed

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const menuContainer = document.querySelector(".menu-container");
+
+menuToggle.addEventListener("click", () => {
+  menuContainer.classList.toggle("active");
+});
+
+// Close the menu when a link is clicked (optional)
+document.querySelectorAll(".menu a").forEach((link) => {
+  link.addEventListener("click", () => {
+    menuContainer.classList.remove("active");
+  });
+});
