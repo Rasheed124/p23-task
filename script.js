9c1889ea7514607caadd1974011452ebb9ec53c1
Rasheed124 <rashdevatrealglobe@gmail.com>
2025-07-25 10:59:38 +0100

    wrapped  project completed

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ menuToggle.addEventListener("click", () => {
   menuContainer.classList.toggle("active");
 });
 
-// Close the menu when a link is clicked (optional)
 document.querySelectorAll(".menu a").forEach((link) => {
   link.addEventListener("click", () => {
     menuContainer.classList.remove("active");
