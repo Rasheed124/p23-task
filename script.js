474f259b54740b0b5f376e48366e499ad053eaa8
Rasheed124 <rashdevatrealglobe@gmail.com>
2025-07-25 11:59:10 +0100

    wrapped  project completed

--- script.js
+++ script.js
@@ -11,19 +11,17 @@ document.querySelectorAll(".menu a").forEach((link) => {
   });
 });
 
+const tabButtons = document.querySelectorAll(".tab-btn");
+const tabContents = document.querySelectorAll(".tab-content");
 
+tabButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const tabId = button.getAttribute("data-tab");
 
- const tabButtons = document.querySelectorAll('.tab-btn');
-  const tabContents = document.querySelectorAll('.tab-content');
+    tabButtons.forEach((btn) => btn.classList.remove("active"));
+    tabContents.forEach((content) => content.classList.remove("active"));
 
-  tabButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      const tabId = button.getAttribute('data-tab');
-
-      tabButtons.forEach(btn => btn.classList.remove('active'));
-      tabContents.forEach(content => content.classList.remove('active'));
-
-      button.classList.add('active');
-      document.getElementById(tabId).classList.add('active');
-    });
+    button.classList.add("active");
+    document.getElementById(tabId).classList.add("active");
   });
+});
