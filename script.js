61b7ae81414ea530061e806bb7d3d6c42c45bb2f
Rasheed124 <rashdevatrealglobe@gmail.com>
2025-07-25 09:41:27 +0100

    wraping project

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const menuToggle = document.querySelector(".menu-toggle");
-const menuContainer = document.querySelector(".menu-container ul");
+const menuContainer = document.querySelector(".main-menu-container");
 
 menuToggle.addEventListener("click", () => {
   menuContainer.classList.toggle("active");
@@ -11,3 +11,20 @@ document.querySelectorAll(".menu a").forEach((link) => {
     menuContainer.classList.remove("active");
   });
 });
+
+
+
+ const tabButtons = document.querySelectorAll('.tab-btn');
+  const tabContents = document.querySelectorAll('.tab-content');
+
+  tabButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const tabId = button.getAttribute('data-tab');
+
+      tabButtons.forEach(btn => btn.classList.remove('active'));
+      tabContents.forEach(content => content.classList.remove('active'));
+
+      button.classList.add('active');
+      document.getElementById(tabId).classList.add('active');
+    });
+  });
